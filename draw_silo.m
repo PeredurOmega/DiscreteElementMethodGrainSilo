8e067297a283a0f786e19da4f88c6e911e11711a
pauls <paul.souteyrat@timeplanner-app.com>
2020-05-20 10:44:55 +0200

    Saving and loading data feature

---
+++ draw_silo.m
@@ -0,0 +1,15 @@
+function [] = draw_silo(y_left,y_right,vertical_silo_radius,flow_silo_radius,vertical_silo_height)
+%DRAW_SILO Draw the shape of the silo
+%Dessin du silo
+fplot(y_left, [-vertical_silo_radius -flow_silo_radius],'Color','red');
+hold on
+fplot(y_right, [flow_silo_radius vertical_silo_radius],'Color','red');
+hold on
+line([vertical_silo_radius vertical_silo_radius], [y_right(vertical_silo_radius) y_right(vertical_silo_radius)+vertical_silo_height],'Color','red')
+hold on
+line([-vertical_silo_radius -vertical_silo_radius], [y_left(-vertical_silo_radius) y_left(-vertical_silo_radius)+vertical_silo_height],'Color','red')
+hold on
+grid
+axis equal
+end
+
